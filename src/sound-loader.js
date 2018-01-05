22adf8d641b8f7846876acf16164a45cd2cdcafa
wiz-code <masa@wiz-code.digick.jp>
2018-01-06 01:32:19 +0900

    add es6 shim

--- src/sound-loader.js
+++ src/sound-loader.js
@@ -1,5 +1,5 @@
 /*
- * SoundLoader.js 0.0.3
+ * SoundLoader.js 0.1.0
  * 
  * SoundJSライブラリ使用の音楽ファイル簡易ローダー
  * 
@@ -54,6 +54,28 @@
  * 
  * createjs.Sound.play('sound-1');
 */
+if (!Array.prototype.find) {
+    Array.prototype.find = function(predicate) {
+        if (this === null) {
+            throw new TypeError('Array.prototype.find called on null or undefined');
+        }
+        if (typeof predicate !== 'function') {
+            throw new TypeError('predicate must be a function');
+        }
+        var list = Object(this);
+        var length = list.length >>> 0;
+        var thisArg = arguments[1];
+        var value;
+
+        for (var i = 0; i < length; i++) {
+            value = list[i];
+            if (predicate.call(thisArg, value, i, list)) {
+                return value;
+            }
+        }
+        return undefined;
+    };
+}
 
 ;(function (global) {
     'use strict';
